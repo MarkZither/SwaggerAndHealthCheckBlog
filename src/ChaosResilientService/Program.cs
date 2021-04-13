e95008e330bb74c1fc920a5cb6d2fc2829a9fde4
MarkZither <mark.burton@zither-it.co.uk>
2021-04-13 21:20:30 +0200

    add ChaosResilientService to demo Polly and Simmy

--- src/ChaosResilientService/Program.cs
+++ src/ChaosResilientService/Program.cs
@@ -1,10 +1,18 @@
+using ChaosResilientService.Data;
+using ChaosResilientService.DataAccess;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Serilog;
+using Serilog.Events;
+
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +20,46 @@ namespace ChaosResilientService
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables()
+            .Build();
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //https://github.com/serilog/serilog-settings-configuration
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(Configuration)
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            //.Enrich.FromLogContext()
+            //.WriteTo.Console()
+            .CreateLogger();
+
+            try
+            {
+                Log.Verbose("Verbose enabled");
+                Log.Debug("init main");
+                // https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-5.0
+                var host = CreateHostBuilder(args).Build();
+
+                CreateDbIfNotExists(host);
+
+                host.Run();
+            }
+            catch (Exception exception)
+            {
+                //Serilog: catch setup errors
+                Log.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,6 +67,28 @@ namespace ChaosResilientService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) =>
+                {
+                    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+                }); // Serilog: Setup Serilog for Dependency injection;;
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
     }
 }
--- src/ChaosResilientService/Startup.cs
+++ src/ChaosResilientService/Startup.cs
@@ -1,7 +1,10 @@
+using ChaosResilientService.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,6 +36,8 @@ namespace ChaosResilientService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("InMemory"));
 
             services.AddControllers();
 
