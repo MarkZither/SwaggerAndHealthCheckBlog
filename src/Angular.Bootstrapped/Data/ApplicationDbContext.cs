1328a22e9616b3975b9b24d09313cb71fd75e8fc
MarkZither <mark.burton@zither-it.co.uk>
2021-11-07 18:15:56 +0100

    add a Angular frontend using the bootstrapped template

---
+++ src/Angular.Bootstrapped/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Angular.Bootstrapped.Models;
+
+using Duende.IdentityServer.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Angular.Bootstrapped.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
