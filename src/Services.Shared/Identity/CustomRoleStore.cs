3fb64e067b1a2c2b0e1906d98ef6f3476074fa63
MarkZither <mark.burton@zither-it.co.uk>
2023-02-22 21:27:18 +0100

    Add unimplemented CustomRoleStore

---
+++ src/Services.Shared/Identity/CustomRoleStore.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Services.Shared.Identity
+{
+    internal class CustomRoleStore<TRole> : IRoleStore<TRole> where TRole : IdentityRole
+    {
+        public Task<IdentityResult> CreateAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TRole> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TRole> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedRoleNameAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleIdAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetNormalizedRoleNameAsync(TRole role, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetRoleNameAsync(TRole role, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(TRole role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
